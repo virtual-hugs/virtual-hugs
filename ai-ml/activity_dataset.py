eafaa7a6be21c3c9668419c5bfe5275b291e7a8c
Iana Apolinskaia <apolinskaaana@gmail.com>
2026-09-06 17:20:29 -0400

    created a databest for 5 simple things for future "match"

---
+++ ai-ml/activity_dataset.py
@@ -0,0 +1,43 @@
+activities = {
+    "Breathing Exercise": {
+        "relaxing": 5,
+        "fun": 1,
+        "reflective": 2,
+        "short": 5
+    },
+
+    "Positive Affirmations": {
+        "relaxing": 4,
+        "fun": 2,
+        "reflective": 3,
+        "short": 5
+    },
+
+    "Journaling": {
+        "relaxing": 4,
+        "fun": 2,
+        "reflective": 5,
+        "short": 2
+    },
+
+    "Mini Puzzle": {
+        "relaxing": 2,
+        "fun": 5,
+        "reflective": 2,
+        "short": 4
+    },
+
+    "Gratitude Garden": {
+        "relaxing": 4,
+        "fun": 4,
+        "reflective": 5,
+        "short": 3
+    },
+
+    "Mood Coloring": {
+        "relaxing": 4,
+        "fun": 5,
+        "reflective": 3,
+        "short": 3
+    }
+}
